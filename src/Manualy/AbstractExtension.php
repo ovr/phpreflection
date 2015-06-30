9f5060bb79dd45d51fa766e4a35cf181cc4b1ae3
Dmitry Patsura <talk@dmtry.me>
2015-07-01 01:10:59 +0900

    Added getReflection

--- src/Manualy/AbstractExtension.php
+++ src/Manualy/AbstractExtension.php
@@ -3,16 +3,34 @@
 namespace Ovr\PHPReflection\Manually;
 
 use Ovr\PHPReflection\FunctionReflection;
+use ReflectionExtension;
 
 class AbstractExtension
 {
+    /**
+     * @var string
+     */
     protected $name = 'unknown';
 
+    /**
+     * @var array
+     */
     protected $functions = array();
 
+    /**
+     * @return bool
+     */
     public function isEnabled()
     {
         return extension_loaded($this->name);
+    }
+
+    /**
+     * @return ReflectionExtension
+     */
+    public function getReflection()
+    {
+        return new ReflectionExtension($this->name);
     }
 
     /**
