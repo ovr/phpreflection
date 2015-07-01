9df277fd31ed2a095225171a90d9e79ceabeb913
Dmitry Patsura <talk@dmtry.me>
2015-07-01 15:10:15 +0900

    FunctionReflection implement getNumberOfParameters(), getNumberOfRequiredParameters()

--- sandbox.php
+++ sandbox.php
@@ -7,3 +7,7 @@ include_once __DIR__ . '/vendor/autoload.php';
 $reflector = new Reflector(Reflector::manuallyFactory());
 $functionDefinition = $reflector->getFunction('floatval');
 var_dump($functionDefinition);
+
+$reflection = new ReflectionFunction('floatval');
+var_dump($reflection->getNumberOfParameters());
+var_dump($reflection->getNumberOfRequiredParameters());
--- src/FunctionReflection.php
+++ src/FunctionReflection.php
@@ -19,4 +19,36 @@ class FunctionReflection
         $this->returnType = $returnType;
         $this->returnPossibleValue = $returnPossibleValue;
     }
+
+    /**
+     * Gets number of parameters
+     *
+     * @return int
+     */
+    public function getNumberOfParameters()
+    {
+        return count($this->parameters);
+    }
+
+    /**
+     * Gets number of required parameters
+     *
+     * @return int
+     */
+    public function getNumberOfRequiredParameters()
+    {
+        if (count($this->parameters) == 0) {
+            return 0;
+        }
+
+        $count = 0;
+
+        foreach ($this->parameters as $parameter) {
+            if (isset($parameter['required']) && $parameter['required']) {
+                $count++;
+            }
+        }
+
+        return $count;
+    }
 }
