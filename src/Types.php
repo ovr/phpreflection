720ce0b4235cdf7b55df3364a33df361a907224f
Dmitry Patsura <talk@dmtry.me>
2015-07-01 13:39:28 +0900

    Bitmask Types

--- src/Types.php
+++ src/Types.php
@@ -4,23 +4,25 @@ namespace Ovr\PHPReflection;
 
 class Types
 {
-    const INT_TYPE = 1;
+    const INT_TYPE = 1 << 1;
 
-    const DOUBLE_TYPE = 2;
+    const DOUBLE_TYPE = 1 << 2;
 
-    const STRING_TYPE = 3;
+    const STRING_TYPE = 1 << 3;
 
     const NUMBER = self::INT_TYPE | self::DOUBLE_TYPE;
 
-    const BOOLEAN_TYPE = 4;
+    const BOOLEAN_TYPE = 1 << 4;
 
-    const ARRAY_TYPE = 5;
+    const ARRAY_TYPE = 1 << 5;
 
-    const RESOURCE_TYPE = 6;
+    const RESOURCE_TYPE = 1 << 6;
 
-    const OBJECT_TYPE = 7;
+    const OBJECT_TYPE = 1 << 7;
 
-    const VOID_TYPE = 8;
+    const VOID_TYPE = -1;
 
-    const MIXED = self::INT_TYPE | self::DOUBLE_TYPE | self::STRING_TYPE | self::BOOLEAN_TYPE;
+    const UNKNOWN_TYPE = -2;
+
+    const MIXED = self::INT_TYPE | self::DOUBLE_TYPE | self::STRING_TYPE | self::BOOLEAN_TYPE | self::ARRAY_TYPE | self::RESOURCE_TYPE | self::OBJECT_TYPE;
 }
