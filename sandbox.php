db60eb638c4bafdc643d8d7469007c5a37200bd4
Dmitry Patsura <talk@dmtry.me>
2015-07-01 15:11:51 +0900

    Write...

--- sandbox.php
+++ sandbox.php
@@ -5,9 +5,10 @@ use Ovr\PHPReflection\Reflector;
 include_once __DIR__ . '/vendor/autoload.php';
 
 $reflector = new Reflector(Reflector::manuallyFactory());
-$functionDefinition = $reflector->getFunction('floatval');
-var_dump($functionDefinition);
+$freflection = $reflector->getFunction('floatval');
+var_dump($freflection->getNumberOfParameters());
+var_dump($freflection->getNumberOfRequiredParameters());
 
-$reflection = new ReflectionFunction('floatval');
-var_dump($reflection->getNumberOfParameters());
-var_dump($reflection->getNumberOfRequiredParameters());
+$freflection = new ReflectionFunction('floatval');
+var_dump($freflection->getNumberOfParameters());
+var_dump($freflection->getNumberOfRequiredParameters());
