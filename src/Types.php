e202980fbd5db0fc8cd5d97b76b8ac8f272427a5
Dmitry Patsura <talk@dmtry.me>
2015-07-01 13:36:01 +0900

    Added more Types

--- src/Types.php
+++ src/Types.php
@@ -10,7 +10,17 @@ class Types
 
     const STRING_TYPE = 3;
 
+    const NUMBER = self::INT_TYPE | self::DOUBLE_TYPE;
+
     const BOOLEAN_TYPE = 4;
 
-    const MIXED = self::INT_TYPE || self::DOUBLE_TYPE || self::STRING_TYPE || self::BOOLEAN_TYPE;
+    const ARRAY_TYPE = 5;
+
+    const RESOURCE_TYPE = 6;
+
+    const OBJECT_TYPE = 7;
+
+    const VOID_TYPE = 8;
+
+    const MIXED = self::INT_TYPE | self::DOUBLE_TYPE | self::STRING_TYPE | self::BOOLEAN_TYPE;
 }
