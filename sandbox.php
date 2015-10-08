eee81ac658f0de424753b6b66e2f3f8f17f4775d
Dmitry Patsura <talk@dmtry.me>
2015-10-08 20:37:10 +0300

    Sandbox.php - remove unused code

--- sandbox.php
+++ sandbox.php
@@ -5,11 +5,8 @@ use Ovr\PHPReflection\Reflector;
 include_once __DIR__ . '/vendor/autoload.php';
 
 $reflector = new Reflector(Reflector::manuallyFactory());
-$reflection = $reflector->getFunction('floatval');
+$reflection = $reflector->getFunction('gettype');
+var_dump($reflection);
 var_dump($reflection->getNumberOfParameters());
 var_dump($reflection->getNumberOfRequiredParameters());
 var_dump($reflection->run(array(1)));
-
-$reflection = new ReflectionFunction('floatval');
-var_dump($reflection->getNumberOfParameters());
-var_dump($reflection->getNumberOfRequiredParameters());
